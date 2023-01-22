77d7e372619de3886ea480590dfb9c3abed8cc1b
classicpauldev <1337@classicpaul.dev>
2023-01-22 14:13:00 +0100

    Extract cipher logic into cipher.py module

---
+++ cipher.py
@@ -0,0 +1,19 @@
+"""Caesar cipher encoding and decoding."""
+
+ALPHABET = "qwertyuiopasdfghjklzxcvbnm"
+
+
+def transform(text: str, shift: int) -> str:
+    """Apply Caesar cipher transformation."""
+    result = ""
+    for letter in text:
+        if letter.isupper():
+            if letter.lower() in ALPHABET:
+                idx = (ALPHABET.index(letter.lower()) + shift) % len(ALPHABET)
+                result += ALPHABET[idx].upper()
+        elif letter.lower() in ALPHABET:
+            idx = (ALPHABET.index(letter) + shift) % len(ALPHABET)
+            result += ALPHABET[idx]
+        else:
+            result += letter
+    return result
