bd369969ad4850d92c3e7c307469818ba864c068
classicpauldev <1337@classicpaul.dev>
2023-02-11 12:27:00 +0100

    Add roundtrip test for encrypt/decrypt

--- tests/test_cipher.py
+++ tests/test_cipher.py
@@ -10,3 +10,8 @@ def test_encrypt_basic():
 
 def test_decrypt_basic():
     assert decrypt("itssg", 3) == "hello"
+
+
+def test_roundtrip():
+    text = "Hello World"
+    assert decrypt(encrypt(text, 5), 5) == text
